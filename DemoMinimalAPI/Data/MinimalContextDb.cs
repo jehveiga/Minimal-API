77d0e0f250ff3a7b013df9b53f1841f9f575327f
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-02-05 10:05:17 -0300

    Ädd class DbContext and EntityFrameWork dependencies

---
+++ DemoMinimalAPI/Data/MinimalContextDb.cs
@@ -0,0 +1,33 @@
+﻿using DemoMinimalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoMinimalAPI.Data
+{
+    public class MinimalContextDb : DbContext
+    {
+        public MinimalContextDb(DbContextOptions<MinimalContextDb> contextOptions) : base(contextOptions) {}
+
+        public DbSet<Provider> Providers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Provider>()
+                .HasKey(p => p.Id);
+
+            modelBuilder.Entity<Provider>()
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            modelBuilder.Entity<Provider>()
+                .Property(p => p.Document)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            modelBuilder.Entity<Provider>()
+                .ToTable("Providers");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

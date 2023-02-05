1f279286cd61399a29708bcb685b381bfb156447
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-02-05 11:27:21 -0300

    Create method Get to fetch by id a provider

--- DemoMinimalAPI/Program.cs
+++ DemoMinimalAPI/Program.cs
@@ -1,6 +1,6 @@
 using DemoMinimalAPI.Data;
+using DemoMinimalAPI.Models;
 using Microsoft.EntityFrameworkCore;
-using System.Reflection.Metadata.Ecma335;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,11 +24,25 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Definition first the rote -> mode Async -> parameters -> action
 // Mapping the Get Verb to fetch a list of providers 
 app.MapGet("/provider", async (
     MinimalContextDb context) => 
     await context.Providers.ToListAsync())
     .WithName("GetProvider")
+    .WithTags("Provider");
+
+// Mapping the Get Verb to fetch by id a provider
+app.MapGet("/provider/{id}", async (
+    MinimalContextDb context, 
+    Guid id) =>
+    await context.Providers.FindAsync(id)
+        is Provider provider 
+            ? Results.Ok(provider)
+            : Results.NotFound())
+    .Produces<Provider>(StatusCodes.Status200OK) //Add especification the documentation to API
+    .Produces(StatusCodes.Status404NotFound)     //Add especification the documentation to API
+    .WithName("GetProviderById")
     .WithTags("Provider");
 
 app.Run();

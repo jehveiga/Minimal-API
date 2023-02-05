81a75be2e9927c02f08d07758d95dad13666d764
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-02-05 09:51:03 -0300

    Create class model Provider

---
+++ DemoMinimalAPI/Models/Provider.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoMinimalAPI.Models
+{
+    public class Provider
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Document { get; set; }
+        public bool Active { get; set; }
+    }
+}
